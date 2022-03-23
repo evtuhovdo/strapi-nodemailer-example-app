2122c4e96f5fd20473dcb116c2465cd72e0ea9e6
Denis Evtukhov <evtuhovdo@gmail.com>
2022-03-23 14:02:03 +0700

    FIX EMAIL CONFIG

--- config/plugins.js
+++ config/plugins.js
@@ -3,19 +3,21 @@
 module.exports = ({env}) => {
   return ({
     email: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: env('EMAIL_SMTP_HOST', 'smtp.example.com'),
-        port: env('EMAIL_SMTP_PORT', 587),
-        auth: {
-          user: env('EMAIL_SMTP_USER'),
-          pass: env('EMAIL_SMTP_PASS'),
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('EMAIL_SMTP_HOST', 'smtp.example.com'),
+          port: env('EMAIL_SMTP_PORT', 587),
+          auth: {
+            user: env('EMAIL_SMTP_USER'),
+            pass: env('EMAIL_SMTP_PASS'),
+          },
         },
-      },
-      settings: {
-        defaultFrom: env('EMAIL_ADDRESS_FROM'),
-        defaultReplyTo: env('EMAIL_ADDRESS_REPLY'),
-      },
+        settings: {
+          defaultFrom: env('EMAIL_ADDRESS_FROM'),
+          defaultReplyTo: env('EMAIL_ADDRESS_REPLY'),
+        },
+      }
     },
   });
 };
